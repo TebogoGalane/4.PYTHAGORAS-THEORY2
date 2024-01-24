6fa448a115b1c6b8f252276db1fe7f759c8a70d4
Lenovo-User <Lenovo-User@DESKTOP-QJ3UI1M>
2024-01-24 17:19:13 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4.PYTHAGORAS_THEORY2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a,b,c;
+
+            
+            
+
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+
+            c = Math.Sqrt (a * 2 + b * 2) * 0.5;
+
+            textBox3.Text = c.ToString();
+        }
+    }
+}
